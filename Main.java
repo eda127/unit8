22f1a3dc35f74e3c7d316753ab67a42b3d72991f
eda127 <127062@epsstudents.org>
2025-03-07 14:28:43 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main( String args[])
+  {
+    Picture pix = new Picture("C:\\Users\\student\\.vscode\\.vscode\\u8-PhotoEditor\\Unit 8 Project Starter Code\\images\\flowers1.jpg");
+    pix.zeroGreen();
+    pix.zeroRed();
+    pix.explore();
+
+  }
+}
