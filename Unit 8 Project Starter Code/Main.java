444392431c36ba5060f1959d6bfc4db1f86b374c
eda127 <127062@epsstudents.org>
2025-03-11 14:27:38 -0400

    NEW CODE

---
+++ Unit 8 Project Starter Code/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main( String args[])
+  {
+    Picture pix = new Picture("C:\\Users\\student\\.vscode\\.vscode\\u8-PhotoEditor\\Unit 8 Project Starter Code\\images\\water.jpg");
+    
+    // pix.negative();
+    pix.fixUnderWater();
+    
+    pix.explore();
+
+  }
+}
